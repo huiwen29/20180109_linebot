ec7dac78e1b84aaa85aa44e8ee3830d14635bffd
huiwen29 <huiwen29@users.noreply.github.com>
2018-01-09 11:56:25 +0800

    Create receive.php

---
+++ receive.php
@@ -0,0 +1,8 @@
+<?php
+  $json_str + file_get_contents('php://input'); //接受REQUEST的BODY
+  $json_obj = json_decode($json_str); //轉JSON格式
+
+  $myfile = fopen("log.txt","w+") or die("Unable to open file!");//設定一個log.txt來印訊息
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前加入 \xEF\xBB\xBF轉成utf8格式
+  fclose($myfile);
+?>
