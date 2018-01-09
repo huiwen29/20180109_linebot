da1b01ece3c58f63afdbbb73db43bcc47ea6473b
huiwen29 <huiwen29@users.noreply.github.com>
2018-01-09 11:59:41 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,3 +1,8 @@
 <?php
-  hi
+  $json_str = file_get_contents('php://input');
+	$json_obj = json_decode($json_str);
+	$myfile = fopen("log.txt", "w+") or die("unable open file");
+
+	fwrite($myfile, "\xEF\xBB\xBF".$json_str);
+	fclose($myfile);
 ?>
